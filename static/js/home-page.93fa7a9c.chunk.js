d766bb46ec00374a5252c40322c31db2a1aad913
Anton Vodorez <vodorez.anton07@gmail.com>
2021-03-22 04:28:47 +0200

    Updates

---
+++ static/js/home-page.93fa7a9c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[1],{122:function(t,e,n){t.exports={container:"StartView_container__3f401",title:"StartView_title__Lm_jf",titleSpan:"StartView_titleSpan__2Ohuo"}},126:function(t,e,n){"use strict";n.r(e);n(0);var i=n(122),o=n.n(i),a=n(1);e.default=function(){return Object(a.jsx)("div",{className:o.a.container,children:Object(a.jsx)("h1",{className:o.a.title,children:"Phonebook App"})})}}}]);
+//# sourceMappingURL=home-page.93fa7a9c.chunk.js.map
